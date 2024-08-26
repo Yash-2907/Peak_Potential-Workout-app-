9280d6c7d5ccdc870f6c226027369d579fa06cb2
Yash-2907 <guptayp_1@rknec.edu>
2024-08-27 01:13:22 +0530

    binding

--- app/src/main/java/com/example/peakpotential/MainActivity.kt
+++ app/src/main/java/com/example/peakpotential/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.peakpotential
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.peakpotential.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bind:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
     }
 }
